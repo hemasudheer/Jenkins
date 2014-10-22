0d1361383c7dbbb668f44a5b0bb801e403607434
hemasudheer <hsksudheer@gmail.com>
2014-10-22 09:49:41 +0100

    Update usingpprint.py

--- usingpprint.py
+++ usingpprint.py
@@ -1,1 +1,8 @@
-import pp
+import pprint
+
+cars = { "peugot" : 307 ,
+         "vauxhall" : "astra", 
+         "lamborghini": "gallardo",
+         "bugatti": "veyron" }
+         
+pprint.pprint(obj, cars, indent= 20)
